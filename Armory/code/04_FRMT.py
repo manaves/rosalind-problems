ccd78efe3512900968a5a51d9ca96c90044267e8
manaves <navarroparedesmaria@gmail.com>
2025-07-10 13:28:18 +0200

    comments added

--- Armory/code/04_FRMT.py
+++ Armory/code/04_FRMT.py
@@ -24,20 +24,41 @@ Example:
 from Bio import SeqIO, Entrez
 
 def search_shortest(records):
+    """
+    Searches for the shortest sequence in a list of GenBank records.
+    
+    Parameters:
+        records (list): a list of SeqRecord objects from Biopython.
+    
+    Returns:
+        SeqRecord: the SeqRecord object with the shortest sequence.
+    """
     lengths = []
+    # Calculate the length of each sequence and store it in a list
     for record in records:
         sequence = record.seq
         lengths.append(len(sequence))
+    # Find the index of the minimum length in the lengths list
     idx_min = lengths.index(min(lengths))
-    
     min_record = records[idx_min]
     
     return min_record
 
 def get_sequences(ids):
+    """
+    Fetches GenBank records from the Nucleotide database using a list of IDs.
+    
+    Parameters:
+        ids (list): a list of GenBank entry IDs.
+        
+    Returns:
+        list: a list of SeqRecord objects containing the fetched sequences.
+    """
     Entrez.email = "manaves@uoc.edu"
     
-    # Construct the search term with the genus name and date range
+    # Construct the search term with the IDs
+    # The IDs are joined by commas to form a single search term
+    # Example: "FJ817486,JX069768,JX469983"
     term = ""
     for id in ids:
         if id == ids[-1]:
@@ -45,6 +66,7 @@ def get_sequences(ids):
         else:
             term += f'{id},'
     search_term = [term]
+    # Fetch the records from the Nucleotide database using the search term
     handle = Entrez.efetch(db='nucleotide', id=search_term, rettype='fasta')
     records = list (SeqIO.parse(handle, "fasta"))
     handle.close()
